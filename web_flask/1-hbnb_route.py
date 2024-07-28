e260f4b70a964d9a877623c24aebdd5c1f11e4e8
chakr0une <soufianbenchakroune7@gmail.com>
2024-07-28 16:57:56 +0000

    test task1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,26 @@
-pt that starts a Flask web application
-listening on address 0.0.0.0 port 5000
-adds another route to serve /HBNB
+ a Flask web application.
 """
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Function that generates the main route """
+@app.route('/')
+def home():
+    """
+    Displays 'Hello HBNB!'.
+    """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def do_hbnb():
-    """ Function that generates that serves hbhb"""
-    return 'HBNB'
+@app.route('/hbnb')
+def hbnb():
+    """
+    Displays 'HBNB'.
+    """
+    return "HBNB"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)

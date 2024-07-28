65334e25b98335440e4b83bad41a9ffbf68a415a
chakr0une <soufianbenchakroune7@gmail.com>
2024-07-28 16:19:21 +0000

    fix task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,34 @@
-tart Flask application
+#!/usr/bin/python3
+
+"""Script that starts a Flask web application
+listening on address 0.0.0.0 port 5000
+adds another route to serve /HBNB
 """
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def hello_hbnb() -> str:
+    """ Function that generates the main route """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """returns HBNB"""
+def do_hbnb() -> str:
+    """ Function that generates that serves hbhb"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+def custom_url(text) -> str:
+    """ Function that generates that serves hbhb"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return f'C {text}'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
